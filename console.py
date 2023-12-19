9692b758df3d874998fe31574657bde350e7ea39
Chris Halogen <christackoms@gmail.com>
2023-12-19 11:43:30 +0100

    No 2

--- console.py
+++ console.py
@@ -114,7 +114,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Create an object of any class"""
+        """ Create an object of any class - No 2"""
         try:
             if not args:
                 raise SyntaxError()
