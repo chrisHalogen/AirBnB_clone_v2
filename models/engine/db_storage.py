6d8a56452d95b9c1128358de5dad9548bca26fa7
Chris Halogen <christackoms@gmail.com>
2023-12-19 11:24:29 +0100

    Changed the DB and File Storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,93 +1,95 @@
 #!/usr/bin/python3
-""" 
-A New Class for SQL Alchemy
-"""
+""" new class for sqlAlchemy """
 from os import getenv
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker, scoped_session
-
+from sqlalchemy import (create_engine)
 from sqlalchemy.ext.declarative import declarative_base
-from models.user import User
-from models.base_model import BaseModel as Base
-from models.place import Place
-from models.state import State
-from models.review import Review
-from models.city import City
+from models.base_model import Base
 from models.amenity import Amenity
+from models.state import State
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models.city import City
 
 class DBStorage:
     """
-    Creates Tables in the Database
+    Database management
     """
     __engine = None
     __session = None
 
     def __init__(self):
+        """
+        Instantiates the class.
+        Creates a DB COnnection
+        """
         user = getenv("HBNB_MYSQL_USER")
-        env = getenv("HBNB_ENV")
         passwd = getenv("HBNB_MYSQL_PWD")
-        host = getenv("HBNB_MYSQL_HOST")
         db = getenv("HBNB_MYSQL_DB")
-        
-        self.__engine = create_engine(
-            'mysql+mysqldb://{}:{}@{}/{}'
-            .format(user, passwd, host, db),
-            pool_pre_ping=True)
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db),
+                                      pool_pre_ping=True)
 
         if env == "test":
             Base.metadata.drop_all(self.__engine)
 
-    def all(self, model_type=None):
+    def all(self, class_=None):
         """
-        Returns A Dictionary of Objects of class model_type
+        Returns a dictionary of all the objects
+        if class_ is not none, then it returns
+        objects from that class alone
         """
         output = {}
-        if model_type:
-            if type(model_type) is str:
-                model_type = eval(model_type)
-            query = self.__session.query(model_type)
-            for item in query:
-                key = "{}.{}".format(type(item).__name__, item.id)
-                output[key] = item
+        if class_:
+            if type(class_) is str:
+                class_ = eval(class_)
+            query = self.__session.query(class_)
+            for elem in query:
+                key = "{}.{}".format(type(elem).__name__, elem.id)
+                output[key] = elem
         else:
-            models = [State, City, User, Place, Review, Amenity]
-            for model in models:
-                query = self.__session.query(model)
-                for item in query:
-                    key = "{}.{}".format(type(item).__name__, item.id)
-                    output[key] = item
+            classes = [State, City, User, Place, Review, Amenity]
+            for cls in classes:
+                query = self.__session.query(cls)
+                for elem in query:
+                    key = "{}.{}".format(type(elem).__name__, elem.id)
+                    output[key] = elem
         return (output)
 
     def new(self, obj):
         """
-        Create new element in db
+        Creates new entry in DB
         """
         self.__session.add(obj)
 
     def save(self):
         """
-        commit changes to db
+        commits changes to db
         """
         self.__session.commit()
 
     def delete(self, obj=None):
         """
-        remove element from db
+        removes element from db if the obj is not none
         """
         if obj:
             self.session.delete(obj)
 
     def reload(self):
         """
-        reload configuration
+        reload session configurations
         """
         Base.metadata.create_all(self.__engine)
-        sesn = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(sesn)
+        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sec)
         self.__session = Session()
 
     def close(self):
         """
-        close session
+        close connection
         """
         self.__session.close()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""
+State Module for HBNB project
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+import models
+import shlex
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """
+    The state class
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
+    @property
+    def cities(self):
+        all_cities = models.storage.all()
+        collect = []
+        output = []
+        for key in all_cities:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                collect.append(all_cities[key])
+        for item in collect:
+            if (item.state_id == self.id):
+                output.append(item)
+        return (output)
